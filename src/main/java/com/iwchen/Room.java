66dc730f2b609f68e86499baf3f005d378b90ec1
Ivan Chen <mr.ivan.chen@gmail.com>
2018-06-19 17:17:08 -0500

    update Room.class constructor

--- src/main/java/com/iwchen/Room.java
+++ src/main/java/com/iwchen/Room.java
@@ -1,5 +1,7 @@
 package com.iwchen;
 
+import java.util.*;
+
 public class Room {
 
 public enum RoomType{
@@ -11,7 +13,6 @@ public enum RoomType{
   TWIN,
   KING,
   QUEEN,
-  TRIPLE,
   DOUBLE,
   SINGLE
 }
@@ -26,5 +27,53 @@ public enum RoomType{
 
   public Room(RoomType roomType){
     this.roomType = roomType;
+
+    switch (roomType) {
+      case PENTHOUSE_SUITE:
+        this.price = 499.99;
+        break;
+
+      case MASTER_SUITE:
+        this.price = 399.99;
+        break;
+
+      case MINI_SUITE:
+        this.price = 299.99;
+        break;
+
+      case DOUBLE_DOUBLE:
+        this.price = 169.99;
+        break;
+
+      case TWIN:
+        this.price = 99.99;
+        break;
+
+      case KING:
+        this.price = 129.99;
+        break;
+
+      case QUEEN:
+        this.price = 109.99;
+        break;
+
+      case DOUBLE:
+        this.price = 79.99;
+        break;
+
+      case SINGLE:
+        this.price = 49.99;
+        break;
+    }
+
+    this.isReserved = false;
+    this.startDate = null;
+    this.endDate = null;
+    /**
+    *TODO
+    *roomNumber
+    *occupancy
+    */
+
   }
 }
