c8abb43e2fb22c00e44ad3970c899d7373631cd8
Ivan Chen <mr.ivan.chen@gmail.com>
2018-06-20 17:00:14 -0500

    Add jacoco plugin to pom.xml

--- src/main/java/com/iwchen/Hotel.java
+++ src/main/java/com/iwchen/Hotel.java
@@ -1,6 +1,22 @@
 package com.iwchen;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Hotel {
 
+  private Map<Integer, Room> roomListing;
+
+  public Hotel() {
+    roomListing = new HashMap<>();
+  }
+
+  public Map<Integer, Room> getRoomListing() {
+    return roomListing;
+  }
+
+  public void setRoomListing(Map<Integer, Room> rooms) {
+    this.roomListing = rooms;
+  }
 
 }
--- src/main/java/com/iwchen/Room.java
+++ src/main/java/com/iwchen/Room.java
@@ -4,26 +4,26 @@ import java.util.*;
 
 public class Room {
 
-public enum RoomType{
+  public enum RoomType{
+    PENTHOUSE_SUITE,
+    MASTER_SUITE,
+    MINI_SUITE,
+    DOUBLE_DOUBLE,
+    TWIN,
+    KING,
+    QUEEN,
+    DOUBLE,
+    SINGLE
+  }
 
-  PENTHOUSE_SUITE,
-  MASTER_SUITE,
-  MINI_SUITE,
-  DOUBLE_DOUBLE,
-  TWIN,
-  KING,
-  QUEEN,
-  DOUBLE,
-  SINGLE
-}
-
-  RoomType roomType;
+  final RoomType roomType;
   double price;
   boolean isReserved;
   Date startDate;
   Date endDate;
   int roomNumber;
   int occupancy;
+  final int maxOccupancy;
 
   public Room(RoomType roomType){
     this.roomType = roomType;
@@ -31,39 +31,52 @@ public enum RoomType{
     switch (roomType) {
       case PENTHOUSE_SUITE:
         this.price = 499.99;
+        this.maxOccupancy = 8;
         break;
 
       case MASTER_SUITE:
         this.price = 399.99;
+        this.maxOccupancy = 5;
         break;
 
       case MINI_SUITE:
         this.price = 299.99;
+        this.maxOccupancy = 3;
         break;
 
       case DOUBLE_DOUBLE:
         this.price = 169.99;
+        this.maxOccupancy = 4;
         break;
 
       case TWIN:
         this.price = 99.99;
+        this.maxOccupancy = 2;
         break;
 
       case KING:
         this.price = 129.99;
+        this.maxOccupancy = 2;
         break;
 
       case QUEEN:
         this.price = 109.99;
+        this.maxOccupancy = 2;
         break;
 
       case DOUBLE:
         this.price = 79.99;
+        this.maxOccupancy = 2;
         break;
 
       case SINGLE:
         this.price = 49.99;
+        this.maxOccupancy = 1;
         break;
+
+      default:
+        this.price = 0.0;
+        this.maxOccupancy = 0;
     }
 
     this.isReserved = false;
